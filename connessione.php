b3ed28b2c1bd1e8566f4c5203244162fb9210064
jacopo <jacopoczzs@gmail.com>
2025-03-13 17:09:27 +0100

    Update connessione.php

--- connessione.php
+++ connessione.php
@@ -3,9 +3,11 @@ $servername = getenv("MYSQLHOST");
 $username = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 $dbname = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
+// Controllo della connessione
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
