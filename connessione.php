7965409d23d54143ae5d8644b6cbb1c2fe5d5a65
jacopo-git <jacopoczzs@gmail.com>
2025-03-13 11:25:20 +0100

    Create connessione.php

---
+++ connessione.php
@@ -0,0 +1,12 @@
+<?php
+$servername = getenv("MYSQLHOST");
+$username = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$dbname = getenv("MYSQLDATABASE");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+?>
